cd3718e102be18946859213fe6a7e82f009c2a5b
Favour <efemiayafavour@gmail.com>
2023-11-02 05:16:46 -0700

    done

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,21 +9,22 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newnode;
+	binary_tree_t *new_node;
 
-	if (!parent)
+	if (parent == NULL)
 		return (NULL);
 
-	newnode = binary_tree_node(parent, value);
-	if (!newnode)
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
 		return (NULL);
 
-	if (parent->left)
+	if (parent->left != NULL)
 	{
-		newnode->left = parent->left;
-		parent->left->parent = newnode;
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
 	}
-	parent->left = newnode;
 
-	return (newnode);
+	parent->left = new_node;
+
+	return (new_node);
 }
